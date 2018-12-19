166dd8a7aafc7a98db83893038647a88c5d6206f
LENA EIVY <applena@gmail.com>
2018-12-19 08:28:57 -0800

    added a start script in package.json

--- objects/index.js
+++ objects/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var express = require('express')
-var app = express()
+
+var express = require('express');
+var app = express();
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
