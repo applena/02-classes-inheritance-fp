f4cac4a3e1335a298bbaa657dde1a80ebf1ac563
LENA EIVY <applena@gmail.com>
2018-12-19 07:26:01 -0800

    now with a travisw.yml file

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -27,7 +27,7 @@ describe('List Data Structure', () => {
     stuff.shift();
     expect(stuff.length).toEqual(1);
     expect(stuff.data[0]).toEqual('b');
-  })
+  });
 
   it('puts an item at the front of the array', () => {
     let stuff = new List();
@@ -36,7 +36,7 @@ describe('List Data Structure', () => {
     stuff.unshift('z');
     expect(stuff.length).toEqual(3);
     expect(stuff.data[0]).toEqual('z');
-  })
+  });
 
   it('starts at an index and removes the number of specified items then puts in the new item at that location', () => {
     let stuff = new List();
@@ -46,13 +46,14 @@ describe('List Data Structure', () => {
     stuff.splice(1, 1, 'z');
     expect(stuff.length).toEqual(3);
     expect(stuff.data[1]).toEqual('z');
-  })
+  });
+
   it('throws an error when arguments are invalid', () => {
     let stuff = new List();
     expect(()=>{
-      stuff.splice('zip', '23')
+      stuff.splice('zip', '23');
     }).toThrow();
-  })
+  });
 
   it('starting at the first param it returns a new obj from that point on or until the second param', () => {
     let stuff = new List();
@@ -60,6 +61,6 @@ describe('List Data Structure', () => {
     stuff.push('b');
     stuff.push('c');
     expect(stuff.slice(1,2)[0]).toEqual('b');
-  })
+  });
 
 });
